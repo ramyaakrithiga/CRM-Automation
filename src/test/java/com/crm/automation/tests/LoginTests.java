01c497c93086ccc9f91f290720803fcb0355e2bd
Ramya <your_email@example.com>
2026-08-24 22:26:31 +0530

    refactor: updated test logic and CRM automation code

--- src/test/java/com/crm/automation/tests/LoginTests.java
+++ src/test/java/com/crm/automation/tests/LoginTests.java
@@ -22,7 +22,7 @@ import com.crm.automation.utilities.CommonUtils;
 import com.crm.automation.utilities.DriverManager;
 
 /**
- * Login Test Cases
+ * Login Test Cases for CRM
  */
 public class LoginTests extends BaseTest {
     private static final Logger logger = LogManager.getLogger(LoginTests.class);
